68e6197f83a130cb7104a4189a665e2bafc03ee8
phantommik3 <miguelforex77@gmail.com>
2023-06-09 20:36:37 -0400

    staging changes and adding new file completed task

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None:
+        return (0)
+
+    result = 0
+    r_data = {'I': 1, 'V': 5, 'X': 10,
+              'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    r_list = list(roman_string.upper())
+
+    for value in r_list:
+        result += r_data[value]
